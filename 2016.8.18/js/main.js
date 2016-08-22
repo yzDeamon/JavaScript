a2bea5a96049cedefece0441c208a04737e2feff
烟箐不在家 <940802753@qq.com>
2016-08-22 08:47:39 +0800

    JavaScript
    2016.08.18

---
+++ 2016.8.18/js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Administrator on 2016/8/18 0018.
+ */
+window.onload=function(){
+    var oLi = document.getElementById("Li-txt");
+    var oTxt = document.getElementById("text");
+    var oBtn = document.getElementById("btn");
+    var oName = document.getElementById("name");
+    oName.onclick=function(){
+		if(this.innerHTML=="猴子："){
+		    this.innerHTML="唐僧说："
+		}else{
+		    this.innerHTML="猴子：";
+		}
+    };
+    oBtn.onclick=function(){
+	   oLi.innerHTML += "<div class='Li-txt-a'>" + oName.innerHTML + oTxt.value + "</div><br>";
+	   oTxt.value="";
+    };
+
+}
+;
